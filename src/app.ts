80e2f3eb68386b17aab1bf2bd81c40e3f481762f
Joel <agboolajoel123@gmail.com>
2025-11-28 10:15:03 +0100

    feat(app): integrate global error handler middleware in app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,9 @@ import express, { NextFunction, Request , Response}  from "express";
 import morgan from "morgan";
 
 import productRoutes from "./Routes/productRoutes"
-import categoryRoutes from "./Routes/categoriesRoute";
+import categoryRoutes from "./Routes/categoriesRoutes";
 import AppError from "./utils/AppError";
+import { globalErrorHandler } from "./Error/globalErrorHandler";
 
 const app = express()
 app.use(express.urlencoded({ extended: true }));
@@ -18,5 +19,7 @@ app.use('/api/v1/categories', categoryRoutes)
 app.use( (req: Request, res: Response, next: NextFunction)=> {
     next( new AppError(`Can't find ${req.originalUrl} on this server`, 404))
 })
+
+app.use(globalErrorHandler)
 
 export default app

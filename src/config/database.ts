d4698d377ce3c949b4fb78a3a4410dedfaa2a42c
Joel <agboolajoel123@gmail.com>
2025-12-02 17:19:04 +0100

    chore(config): move database file from utils to config and update imports

---
+++ src/config/database.ts
@@ -0,0 +1,59 @@
+import { Pool, PoolClient } from 'pg';
+import  { PrismaClient } from "@prisma/client"
+import dotenv from  "dotenv"
+
+dotenv.config()
+
+export const prisma = new PrismaClient({
+  log: 
+      process.env.NODE_ENV === 'development'
+            ? [ 'query', 'error', 'warn']
+            : [ 'error']
+})
+
+export const connectDB = async () => {
+  try {
+      await prisma.$connect();
+      console.log("🟢 DB Connected  via Prisma")
+  } catch (error) {
+      const message = error instanceof Error ? error.message : String(error);
+      console.error(`DB connection error: ${message}`)
+      process.exit(1)
+  }
+}
+
+export const disconnectDB = async () => {
+  await prisma.$disconnect()
+}
+
+
+
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: Number(process.env.DB_PORT), // default PostgreSQL port
+// });
+
+// pool.connect()
+//   .then(() => {
+//     console.log("🟢 Connected to PostgreSQL");
+//   })
+//   .catch((err) => {
+//     console.error("🔴 PostgreSQL connection error:", err);
+//   });
+
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+// });
+
+// pool.connect()
+//   .then(() => {
+//     console.log("🟢 Connected to PostgreSQL");
+//   })
+//   .catch((err) => {
+//     console.error("🔴 PostgreSQL connection error:", err);
+//   });
+
+

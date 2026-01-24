c67f5480822bc29bd6403550680150fb72597d7b
joeloo1 <agboolajoel123@gmail.com>
2026-01-24 20:29:21 +0100

    style: format code

--- src/utils/AppError.ts
+++ src/utils/AppError.ts
@@ -1,17 +1,25 @@
 class AppError extends Error {
-    public statusCode: number; 
-    public status: string;
-    public isOperational: boolean
+  public statusCode: number;
+  public status: string;
+  public isOperational: boolean;
 
-    constructor(message: string, statusCode: number) {
-        super(message)
-        
-        this.statusCode = statusCode;
-        this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
-        this.isOperational = true
+  constructor(message: string, statusCode: number) {
+    super(message);
 
+<<<<<<< HEAD
         Error.captureStackTrace(this, this.constructor)
     }
 }
 
 export default AppError
+=======
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
+    this.isOperational = true;
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export default AppError;
+>>>>>>> f65b621 (style: format code)
